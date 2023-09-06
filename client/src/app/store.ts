7d485074e5bfd83b981ed4b109a936f02700082d
Sam Serrano <samuel.j.serrano@gmail.com>
2023-09-06 05:56:50 -0700

    linting /app

--- client/src/app/store.ts
+++ client/src/app/store.ts
@@ -1,23 +1,23 @@
-import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
-import profileReducer from '../features/profile/profileSlice';
-import rubricReducer from '../features/rubric/rubricSlice';
-import scoreReducer from '../features/scores/scoreSlice';
+import { configureStore, type ThunkAction, type Action } from '@reduxjs/toolkit'
+import counterReducer from '../features/counter/counterSlice'
+import profileReducer from '../features/profile/profileSlice'
+import rubricReducer from '../features/rubric/rubricSlice'
+import scoreReducer from '../features/scores/scoreSlice'
 
 export const store = configureStore({
   reducer: {
     counter: counterReducer,
     profile: profileReducer,
     rubric: rubricReducer,
-    score: scoreReducer,
-  },
-});
+    score: scoreReducer
+  }
+})
 
-export type AppDispatch = typeof store.dispatch;
-export type RootState = ReturnType<typeof store.getState>;
+export type AppDispatch = typeof store.dispatch
+export type RootState = ReturnType<typeof store.getState>
 export type AppThunk<ReturnType = void> = ThunkAction<
-  ReturnType,
-  RootState,
-  unknown,
-  Action<string>
->;
+ReturnType,
+RootState,
+unknown,
+Action<string>
+>
