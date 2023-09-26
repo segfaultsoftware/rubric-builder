565464a52298dbaf06dc79a79d400e3cb960a5c7
Sam Serrano <samuel.j.serrano@gmail.com>
2023-09-26 14:41:35 -0700

    User can copy from view only page

--- client/src/features/rubric/RubricViewOnly.tsx
+++ client/src/features/rubric/RubricViewOnly.tsx
@@ -1,13 +1,15 @@
 import React, { useEffect } from 'react'
 import { useAppDispatch, useAppSelector } from '../../app/hooks'
-import { useParams } from 'react-router-dom'
+import { Link, useParams } from 'react-router-dom'
 import { fetchRubric, resetRubricState, selectRubric } from './rubricSlice'
 import RubricForm from './RubricForm'
+import { selectLoggedInAs } from '../profile/profileSlice'
 
 const RubricViewOnly = () => {
   const dispatch = useAppDispatch()
   const { rubricId } = useParams()
 
+  const loggedInAs = useAppSelector(selectLoggedInAs)
   const rubric = useAppSelector(selectRubric)
 
   useEffect(() => {
@@ -27,6 +29,13 @@ const RubricViewOnly = () => {
     : (
     <div className='text-center col-md-6 offset-md-3'>
       <RubricForm rubric={rubric} isViewOnly />
+      {loggedInAs && (
+        <div className='row mb-3'>
+          <div className='col-md-12'>
+            <Link className='btn btn-primary' to={`/rubrics/new?copyFromId=${rubric.id}`}>Copy</Link>
+          </div>
+        </div>
+      )}
     </div>
       )
 }
